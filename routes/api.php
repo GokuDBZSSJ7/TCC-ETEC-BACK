02ee2dacbe2c610f378a9960323e0bfe8fae3fdb
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-08-17 18:40:02 -0300

    Feat: Rota de Autenticação

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AuthController::class, 'login']);
+Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('register', [UserController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
