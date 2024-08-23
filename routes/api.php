4d9590a0eaa41ac8eabd9ca76d7d504f0bc13d68
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-08-23 19:22:25 -0300

    Feat: Ajustes

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('register', [UserController::class, 'store']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('me', [AuthController::class, 'me']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    
-});
+Route::get('me', [AuthController::class, 'me']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+
+Route::resource('users', UserController::class);
+Route::middleware('auth:sanctum')->group(function () {});
