88bc89f421d081749570f255347272b23c9ced87
Lucas Baggio <lucas.baggio.silva@gmail.com>
2024-08-26 08:44:25 -0300

    Feat: Rota de partido

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CityController;
+use App\Http\Controllers\PartyController;
 use App\Http\Controllers\StateController;
 use App\Http\Controllers\UserController;
 
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->group(function () {});
 Route::resource('states', StateController::class);
 Route::resource('cities', CityController::class);
 Route::get('getCitieByStateId/{id}', [CityController::class, 'getCitieByStateId']);
+
+Route::resource('parties', PartyController::class);
